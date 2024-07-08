61e00ad292d70543e5471c54c0616256ab9e8965
Binod Rasaili <binod.toyam@gmail.com>
2024-07-08 14:26:46 -0500

    done

--- Lab11Sender/src/main/java/jms/SpringJmsAllSenderApplication.java
+++ Lab11Sender/src/main/java/jms/SpringJmsAllSenderApplication.java
@@ -1,35 +1,24 @@
 package jms;
 
-import jakarta.jms.ConnectionFactory;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.jms.annotation.EnableJms;
 import org.springframework.jms.core.JmsTemplate;
-import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
-import org.springframework.jms.support.converter.MessageConverter;
-import org.springframework.jms.support.converter.MessageType;
-
-import java.util.Optional;
 
 
 @SpringBootApplication
 @EnableJms
-public class SpringJmsPersonSenderApplication implements CommandLineRunner {
+public class SpringJmsAllSenderApplication implements CommandLineRunner {
 	@Autowired
 	JmsTemplate jmsTemplate;
 
 
 	public static void main(String[] args)  {
-		ConfigurableApplicationContext context = SpringApplication.run(SpringJmsPersonSenderApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(SpringJmsAllSenderApplication.class, args);
 		context.close();
 	}
 
@@ -42,6 +31,24 @@ public class SpringJmsPersonSenderApplication implements CommandLineRunner {
 
 		System.out.println("Sending a JMS message:" + personAsString);
 		jmsTemplate.convertAndSend("testQueue",personAsString);
+
+		//For Calculator
+		Calculator cal1=new Calculator(4,"+");
+		Calculator cal2=new Calculator(10,"*");
+		Calculator cal3=new Calculator(2,"9");
+
+		String cal1AsString=objectMapper.writeValueAsString(cal1);
+		String cal2AsString=objectMapper.writeValueAsString(cal2);
+		String cal3AsString=objectMapper.writeValueAsString(cal3);
+
+		System.out.println("Sending Calculator message:" + cal1AsString);
+		jmsTemplate.convertAndSend("calcQueue",cal1AsString);
+
+		System.out.println("Sending Calculator message:" + cal2AsString);
+		jmsTemplate.convertAndSend("calcQueue",cal2AsString);
+
+		System.out.println("Sending Calculator message:" + cal3AsString);
+		jmsTemplate.convertAndSend("calcQueue",cal3AsString);
 
 	}
 
